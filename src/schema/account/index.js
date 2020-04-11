8d7159695057a2f387aae8787c3e452e7253c84f
Nandico Aquino <nandico@gmail.com>
2020-04-11 00:25:15 -0300

    Added account management to GraphQL schema
    Schema received account initial operations.

---
+++ src/schema/account/index.js
@@ -0,0 +1,65 @@
+import { globalIdField } from 'graphql-relay';
+import {
+  GraphQLObjectType,
+  GraphQLNonNull,
+  GraphQLEnumType,
+  GraphQLInt,
+  GraphQLString,
+} from 'graphql';
+import personType from '../person';
+
+export default new GraphQLObjectType({
+  name: 'Account',
+  fields: () => ({
+    id: globalIdField(),
+    person: {
+      type: personType,
+      resolve(parent, _args, { personByAccount }) {
+        return personByAccount.load(parent.id);
+      },
+    },
+    phoneNumber: {
+      type: GraphQLInt,
+      resolve(parent) {
+        return parent.phone_number;
+      },
+    },
+    phoneAreaCode: {
+      type: GraphQLInt,
+      resolve(parent) {
+        return parent.phone_area_code;
+      },
+    },
+    phoneCountryCode: {
+      type: GraphQLInt,
+      resolve(parent) {
+        return parent.phone_country_code;
+      },
+    },
+    email: {
+      type: GraphQLString,
+    },
+    status: {
+      type: new GraphQLEnumType({
+        name: 'status',
+        values: {
+          active: { value: 'active' },
+          pending: { value: 'pending' },
+          disabled: { value: 'disabled' },
+        },
+      }),
+    },
+    createdAt: {
+      type: new GraphQLNonNull(GraphQLString),
+      resolve(parent) {
+        return parent.created_at;
+      },
+    },
+    updatedAt: {
+      type: new GraphQLNonNull(GraphQLString),
+      resolve(parent) {
+        return parent.updated_at;
+      },
+    },
+  }),
+});
