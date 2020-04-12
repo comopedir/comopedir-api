d150132e1ce9bf288bd13d3c2523aaf29f121af9
Nandico Aquino <nandico@gmail.com>
2020-04-12 19:17:36 -0300

    Added single airtable business introspection inside API
    The resolver is going inside airtable to get a single business
    from Airtable.

--- src/schema/airtableBusiness/queries.js
+++ src/schema/airtableBusiness/queries.js
@@ -1,4 +1,4 @@
-import { GraphQLNonNull, GraphQLInt } from 'graphql';
+import { GraphQLNonNull, GraphQLInt, GraphQLString } from 'graphql';
 import {
   connectionDefinitions,
   forwardConnectionArgs,
@@ -7,20 +7,68 @@ import {
 } from 'graphql-relay';
 
 import airtable from '../../services/airtable';
-import ExternalTableType from './index';
+import AirtableBusinessType from './index';
 
 let atb = new airtable();
+
+const airtableBusiness = {
+  type: AirtableBusinessType,
+  args: {
+    airtableId: {
+      type: GraphQLString,
+    },
+  },
+  async resolve(_root, args, context) {
+    const { airtableId } = args;
+
+    let business;
+
+    const [data] = await Promise.all([
+      atb
+        .getTableContents()
+        .then(rows => {
+          rows.forEach(record => {
+
+            if (record['airtableID'] === airtableId) {
+              business = {
+                id: record['airtableID'],
+                airtableId: record['airtableID'],
+                name: record['Nome do Estabelecimento'],
+                services: record['Serviços'],
+                channels: record['Como Pedir'],
+                categories: record['Categoria'],
+                website: record['Website'],
+                whatsapp: record['whatsapp'],
+                phone: record['Telefone para Pedidos'],
+                state: record['Estado'],
+                city: record['Cidade:'],
+                instagram: record['Instagram'],
+                email: record['Email'],
+                approved: record['Aprovado?'],
+                pictures: record['Foto da Comida'],
+              };
+            }
+
+            return record;
+          });
+
+          return business;
+        })]);
+
+    return data;
+  },
+};
 
 const airtableBusinesses = {
   type: connectionDefinitions({
     name: 'ExternalTable',
-    nodeType: ExternalTableType,
+    nodeType: AirtableBusinessType,
     connectionFields: {
       totalCount: { type: new GraphQLNonNull(GraphQLInt) },
     },
   }).connectionType,
   args: forwardConnectionArgs,
-  async resolve(_root, args, ctx) {
+  async resolve(_root, args, _ctx) {
     const offset = args.after ? cursorToOffset(args.after) + 1 : 0;
     const [data, totalCount] = await Promise.all([
       atb
@@ -67,5 +115,6 @@ const airtableBusinesses = {
 };
 
 export default {
+  airtableBusiness,
   airtableBusinesses,
 };
